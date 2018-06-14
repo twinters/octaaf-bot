74be50482ba0461c1f488eb987e80dece5b57451
T. Winters <info@thomaswinters.be>
2018-06-14 02:11:02 +0200

    Added tweet downloading

--- src/main/java/be/thomaswinters/samsonworld/octaaf/experiments/OctaafStoefExperiments.java
+++ src/main/java/be/thomaswinters/samsonworld/octaaf/experiments/OctaafStoefExperiments.java
@@ -1,18 +1,31 @@
 package be.thomaswinters.samsonworld.octaaf.experiments;
 
-import be.thomaswinters.samsonworld.octaaf.OctaafStoefGenerator;
+import be.thomaswinters.twitter.tweetsfetcher.UserTweetsFetcher;
+import be.thomaswinters.twitter.util.TwitterLogin;
+import be.thomaswinters.twitter.util.download.TweetDownloader;
+import twitter4j.TwitterException;
 
+import java.io.File;
 import java.io.IOException;
 
 public class OctaafStoefExperiments {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, TwitterException {
 
-        OctaafStoefGenerator octaaf = new OctaafStoefGenerator();
-        System.out.println(octaaf.generateRelated("Aheuum. Aheuuuum. Aheum.\n" +
-                "Aan allen die luiheid overwinnen: proficiat.\n" +
-                "Aan allen die werklust overwinnen: ook proficiat."));
+//        OctaafStoefGenerator octaaf = new OctaafStoefGenerator();
+//        System.out.println(octaaf.generateRelated("Aheuum. Aheuuuum. Aheum.\n" +
+//                "Aan allen die luiheid overwinnen: proficiat.\n" +
+//                "Aan allen die werklust overwinnen: ook proficiat."));
 
+        downloadTweets();
+
+    }
+
+    private static void downloadTweets() throws IOException, TwitterException {
+        new TweetDownloader(
+                new UserTweetsFetcher(
+                        TwitterLogin.getTwitterFromEnvironment("octaaf."),
+                        "burgemeesterbot",false,true)).downloadTo(new File("burgemeesterbot.txt"));
 
     }
 }
