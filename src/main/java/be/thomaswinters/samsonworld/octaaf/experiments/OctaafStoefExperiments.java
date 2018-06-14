1c68e679d01442d5bc5f5761d6c0211fef958b93
T. Winters <info@thomaswinters.be>
2018-06-14 02:37:06 +0200

    Set up new experiments

--- src/main/java/be/thomaswinters/samsonworld/octaaf/experiments/OctaafStoefExperiments.java
+++ src/main/java/be/thomaswinters/samsonworld/octaaf/experiments/OctaafStoefExperiments.java
@@ -1,23 +1,33 @@
 package be.thomaswinters.samsonworld.octaaf.experiments;
 
+import be.thomaswinters.samsonworld.octaaf.OctaafStoefGenerator;
 import be.thomaswinters.twitter.tweetsfetcher.UserTweetsFetcher;
 import be.thomaswinters.twitter.util.TwitterLogin;
 import be.thomaswinters.twitter.util.download.TweetDownloader;
+import be.thomaswinters.util.DataLoader;
 import twitter4j.TwitterException;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 
 public class OctaafStoefExperiments {
 
     public static void main(String[] args) throws IOException, TwitterException {
 
-//        OctaafStoefGenerator octaaf = new OctaafStoefGenerator();
-//        System.out.println(octaaf.generateRelated("Aheuum. Aheuuuum. Aheum.\n" +
-//                "Aan allen die luiheid overwinnen: proficiat.\n" +
-//                "Aan allen die werklust overwinnen: ook proficiat."));
+        OctaafStoefGenerator octaaf = new OctaafStoefGenerator();
 
-        downloadTweets();
+
+        DataLoader.readLines( "experiments\\gert_bot.txt")
+                .subList(0, 50)
+                .stream()
+                .map(octaaf::generateRelated)
+                .forEach(System.out::println);
+
+        System.out.println(octaaf.generateRelated("Aheuum. Aheuuuum. Aheum. Aan allen die luiheid overwinnen: proficiat. Aan allen die werklust overwinnen: ook proficiat."));
+
+//        downloadTweets();
 
     }
 
@@ -25,7 +35,7 @@ public class OctaafStoefExperiments {
         new TweetDownloader(
                 new UserTweetsFetcher(
                         TwitterLogin.getTwitterFromEnvironment("octaaf."),
-                        "burgemeesterbot",false,true)).downloadTo(new File("burgemeesterbot.txt"));
+                        "gert_bot", false, true)).downloadTo(new File("gert_bot.txt"));
 
     }
 }
